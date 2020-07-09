32dfc09d3f2a4e119ce820ec6d29a82345e469f6
ninja-naveen <68046594+ninja-naveen@users.noreply.github.com>
2020-07-09 11:06:06 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,13 +8,13 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-INDIANBOT_IS_ALIVE = ("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
+INDIANBOT_IS_ALIVE = ("**Ninja Naveen's UserBot** \n`🇳BOT Status : ` **Pro Like Ninja Naveen**\n\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "`Indian Bot Version:` [1.0](https://telegra.ph/INDIAN-06-15-6)\n`Python:` **3.7.4**\n"
-                     "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
-                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
+                     "`Ninja Bot Version:` 2.3\n`Python:` **3.7.4**\n"
+                     "**Join the Bot Creator's Channel** : [💠Royal Giveaways💠](t.me/royal_giveaway)\n"
+                     "**Join the Bot Creator's Group** : [💠Royal Giveaways Chat💠](t.me/royalgiveawaychat)\n\n"
+                     "**USERBOT By** [🔥Ninja Naveen🔥](t.me/NinjaNaveen)\n\n"
+                     "Copyright [Ninja Naveen](t.me/NinjaNaveen)") 
 
 
 #@command(outgoing=True, pattern="^.alive$")
